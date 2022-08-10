3876286d0e7d20fc84e28b1fd94223bc0db74fd8
Jake Anderson <jacob21anderson@gmail.com>
2022-08-09 21:48:16 -0400

    Added dependencies, routing, components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+
+function Header() {
+    return(
+        <div>
+            <h1>Header Component</h1>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Info.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Info () {
+    return(
+        <h1>This is the info page!</h1>
+    );
+};
+
+export default Info;
